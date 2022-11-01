8439702f4f9a668d1df410479139dd61dd86ff7c
Cyprian Tingiweh <cyprian.tingiweh@go-groups.net>
2022-11-01 01:42:14 +0100

    backend for appointment app

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-const app = express();
+const app = express()
 
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(cors(corsOptions));
+const origin = {
+  origin:'*'
+}
 
-// parse requests of content-type - application/json
-app.use(express.json());
+app.use(cors(origin))
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+// Start server
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Server listening`)
+})
 
 const db = require("./app/models");
+
 db.sequelize.sync()
   .then(() => {
     console.log("Synced db.");
@@ -24,20 +28,12 @@ db.sequelize.sync()
     console.log("Failed to sync db: " + err.message);
   });
 
-// // drop the table if it already exists
-// db.sequelize.sync({ force: true }).then(() => {
-//   console.log("Drop and re-sync db.");
-// });
-
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to Patient Appointment application." });
 });
 
-require("./app/routes/turorial.routes")(app);
+require("./app/routes/appointment.routes")(app);
 
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+require("./app/routes/user.routes")(app);
+
